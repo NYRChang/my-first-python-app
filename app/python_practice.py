8f4eeca70e3e84273d654c21c0dba6f9c07ac07d
Richard R. Chang <65420925+NYRChang@users.noreply.github.com>
2020-05-26 20:53:40 -0400

    lecture 2

---
+++ app/python_practice.py
@@ -0,0 +1,30 @@
+
+
+
+
+x = 1
+print(x)    #>1
+
+# breakpoint() # for temporary debugging purposes
+
+x = 10
+print(x)    #>10
+
+y = 5
+print(y)
+
+y = y +100
+print(y)
+
+y += 100 # equiv shorthand for y = y+100
+print(y)
+
+print(1,2,3) #can print many values separated by commas
+
+z = 12
+
+if z == 37:
+    print("YES")
+else:
+    print("NO")
+
